e52d70e5ef99afbf6cf5158c8217f7c329e66696
unknown <aniketkhote99@gmail.com>
2022-01-21 21:48:54 +0530

    medicine module scaffold created

---
+++ app/Models/MedicineTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicineTemplate extends Model
+{
+    use HasFactory;
+}
