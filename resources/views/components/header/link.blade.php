8df8c73ef60764a5cf99a830dd89bdaea3c914ec
unknown <aniketkhote99@gmail.com>
2022-02-07 10:57:12 +0530

    fixed mobile header problem

--- resources/views/components/header/link.blade.php
+++ resources/views/components/header/link.blade.php
@@ -1,7 +1,5 @@
-<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
-    <div class="menu-item me-lg-1">
-        <a class="menu-link py-3" href="{{ $url }}">
-            <span class="menu-title">{{ $name }}</span>
-        </a>
-    </div>
+<div class="menu-item me-lg-1">
+    <a class="menu-link py-3" href="{{ $url }}">
+        <span class="menu-title">{{ $name }}</span>
+    </a>
 </div>
